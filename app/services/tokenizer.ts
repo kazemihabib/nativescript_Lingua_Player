3385b54e4c9490de99fab1d180715f81550ac9b9
habib kazemi <kazemihabib1996@gmail.com>
2016-11-19 01:04:38 +0330

    change return object of tokenizer

--- app/services/tokenizer.ts
+++ app/services/tokenizer.ts
@@ -1,34 +1,52 @@
+enum ILetterType{
+    word,
+    notWord,
+    line
+}
 export class Tokenizer{
     private static isWordSeparator(c){
         return [' ', '.', ',', ';', '!', ':', '?', '@', '%', '^', '&', '=','$','*',
-            '\t', '-', '/', '\\', '|', '"', '”', '”', '<', '>', '\n', '(', ')'].indexOf(c) >= 0;
+            '\t', '-', '/', '\\', '|', '"', '”', '”', '<', '>', '(', ')'].indexOf(c) >= 0;
     };
 
     public static splitWords = function(str){
         let s = str.replace('<br/>', ' ').replace('<br />', ' ');
         s = s.replace('<i>', ' ').replace('</i>', ' ');
-        let ret = [{'text': ''}];
-        let prevLetterIsWordSeparator= false;
+        let ret :{'text':string,'isWord':boolean,'isNotWord':boolean,'isLine':boolean}[] = [];
+        let prevLetterType:ILetterType = null;
         let len = s.length;
         for (let i = 0; i < len; i++) {
             let currentLetter = s[i];
-            let currentLetterIsWordSeparator = this.isWordSeparator(currentLetter);
+            let currentLetterType:ILetterType; 
+            if(this.isWordSeparator(currentLetter))
+               currentLetterType = ILetterType.notWord; 
 
-                /* if current and previous letter has same type */
-                if (currentLetterIsWordSeparator != prevLetterIsWordSeparator){
-                    if (ret[ret.length - 1]['text'] != ''){ 
-                        if (!prevLetterIsWordSeparator) 
-                            ret[ret.length - 1]['isWord'] = true;
-                        else 
-                            ret[ret.length - 1]['isWord'] = false;
-                        ret.push({'text':''});
+            else if(currentLetter == '\n')
+               currentLetterType = ILetterType.line;
+
+            else 
+                currentLetterType = ILetterType.word;
+
+
+
+            /* if current and previous letter has not same type */
+            if (currentLetterType != prevLetterType){
+                console.log('currentLetter ',currentLetter);
+
+                ret.push({'text': '','isWord':false,'isNotWord':false,'isLine':false});
+                    if ( currentLetterType == ILetterType.line)
+                        ret[ret.length - 1]['isLine'] = true;
+
+                    else if (currentLetterType == ILetterType.word){ 
+                        ret[ret.length - 1]['isWord'] = true;
                     }
-                }
-            prevLetterIsWordSeparator = this.isWordSeparator(currentLetter);
-            ret[ret.length - 1]['text'] = ret[ret.length - 1]['text'] + currentLetter
+                    else 
+                        ret[ret.length - 1]['isNotWord'] = true;
+                
+            }
+            prevLetterType = currentLetterType;
+            ret[ret.length - 1]['text'] = ret[ret.length - 1]['text'] + currentLetter;
         }
-        if (! prevLetterIsWordSeparator) ret[ret.length - 1]['isWord'] = true;
-        else ret[ret.length - 1]['isWord'] = false;
 
         return ret;
 
