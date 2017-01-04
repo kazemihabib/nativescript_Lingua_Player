fea47a8768fcae6cec28216976848a7332f8e9f2
habib kazemi <kazemihabib1996@gmail.com>
2017-01-04 10:53:21 +0330

    refactore fileExplorer.service.ts
    break functions into samller peice
    and convert getVideoInfo and setDetailsOfVideoInformations
    to functions.

--- app/services/fileExplorer.service.ts
+++ app/services/fileExplorer.service.ts
@@ -79,52 +79,10 @@ export class FileExplorer {
     private subscriber: any;
 
 
-    private setDetailsOfVideoInformations(mediaRow: any, index: number) {
-        let videoInfo = this.videoInformations[index];
-
-        videoInfo.position = mediaRow.POSITION;
-        videoInfo.length = mediaRow.LENGTH;
-        videoInfo.subLocation = mediaRow.SUBLOCATION;
-        videoInfo.thumbnail = mediaRow.THUMBNAIL;
-
-    }
-
-
-    private getVideoInfo(inx) {
-        let that = this;
-
-        if (inx == this.videoInformations.length) {
-            this.worker.terminate();
-            return this.subscriber.complete();
-        }
-        let path = this.videoInformations[inx].path;
-
-        database.getMediaInfo(path, (err, row) => {
-            if (err) {
-                console.log(err);
-            }
-            if (row) {
-
-                this.setDetailsOfVideoInformations(row, inx);
-                timer.setTimeout(function () { that.getVideoInfo(inx + 1) }, 0);
-
-            }
-            else {
-                this.worker.postMessage(path);
-
-                this.worker.onmessage = (msg) => {
-                    let mediaInfo = msg.data;
-                    this.setDetailsOfVideoInformations(mediaInfo, inx);
-                    timer.setTimeout(function () { that.getVideoInfo(inx + 1) }, 0);
-                }
-            }
-        });
-
-    }
 
 
 
-    public explore() {
+    private mediaStoreQuery(): [VideoInfo[], string[]] {
 
         let MediaStore = android.provider.MediaStore;
         let Uri = android.net.Uri;
@@ -136,41 +94,97 @@ export class FileExplorer {
         let projection = [MediaStore.Video.VideoColumns.DATA];
         let c = application.android.context.getContentResolver().query(uri, projection, null, null, null);
 
+        let videoInformations: VideoInfo[] = [];
+        if (c != null) {
+            let temp;
+            while (temp = c.moveToNext()) {
+                let path: string = c.getString(0); // give path
+                let exists = fs.File.exists(path);
+                if (exists) videoInformations.push(new VideoInfo(path));
+                else notExistVideos.push(path)
+            }
+            c.close();
+
+            return [videoInformations, notExistVideos];
+        }
+
+    }
+
+    private removeNotExistRows(notExistVideos: string[]) {
+        if (notExistVideos.length) {
+            notExistVideos.forEach((path) => {
+                database.deleteRow(path, (err, id) => {
+                    if (err) console.log(err);
+                    if (id) console.log(id);
+                })
+            })
+        }
+
+    }
+
+    public explore() {
+
         if (!database.isDatabaseReady()) {
             database.initDataBase();
         }
 
+        this.worker = new Worker('../workers/setMediaInfo.worker');
+
+        let notExistVideos: string[] = [];
+
+        [this.videoInformations, notExistVideos] = this.mediaStoreQuery();
+        this.removeNotExistRows(notExistVideos);
 
         return RxObservable.create(subscriber => {
             this.subscriber = subscriber;
-            this.worker = new Worker('../workers/setMediaInfo.worker');
+            subscriber.next(this.videoInformations);
 
-            if (c != null) {
-                let temp;
-                while (temp = c.moveToNext()) {
-                    let path: string = c.getString(0); // give path
-                    let exists = fs.File.exists(path);
-                    if (exists) this.videoInformations.push(new VideoInfo(path));
-                    else notExistVideos.push(path)
-                }
-                c.close();
-                subscriber.next(this.videoInformations);
+            let setDetailsOfVideoInformations = (mediaRow: any, index: number) => {
+                let videoInfo = this.videoInformations[index];
+
+                videoInfo.position = mediaRow.POSITION;
+                videoInfo.length = mediaRow.LENGTH;
+                videoInfo.subLocation = mediaRow.SUBLOCATION;
+                videoInfo.thumbnail = mediaRow.THUMBNAIL;
             }
 
-            if (notExistVideos.length) {
-                notExistVideos.forEach((path) => {
-                    database.deleteRow(path, (err, id) => {
-                        if (err) console.log(err);
-                        if (id) console.log(id);
-                    })
-                })
+
+            let getVideoInfo = (inx) => {
+                let that = this;
+
+                if (inx == this.videoInformations.length) {
+                    this.worker.terminate();
+                    return this.subscriber.complete();
+                }
+
+                let path = this.videoInformations[inx].path;
+
+                database.getMediaInfo(path, (err, row) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    if (row) {
+
+                        setDetailsOfVideoInformations(row, inx);
+                        timer.setTimeout(function () { getVideoInfo(inx + 1) }, 0);
+
+                    }
+                    else {
+                        this.worker.postMessage(path);
+
+                        this.worker.onmessage = (msg) => {
+                            let mediaInfo = msg.data;
+                            setDetailsOfVideoInformations(mediaInfo, inx);
+                            timer.setTimeout(function () { getVideoInfo(inx + 1) }, 0);
+                        }
+                    }
+                });
             }
 
             if (this.videoInformations.length) {
-                this.getVideoInfo(0);
+                getVideoInfo(0);
             }
 
         });
-
     }
 }
